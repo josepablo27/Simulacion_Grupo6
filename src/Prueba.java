44340f79e9894ca371ac2158363e2814b6c96f07
josepablo27 <jduran60412@ufide.ac.cr>
2022-07-20 22:44:47 -0600

    Commit 1

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,4 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Prueba {
-
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.automationexercise.com/brand_products/Biba");
+	}
 }
