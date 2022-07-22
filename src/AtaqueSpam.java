ae7e72848b232d2e185d727126ac4ee9e2bec739
AndresDiazGarro <iandresfy@gmail.com>
2022-07-22 01:36:09 -0600

    Ataque Spam 100%

--- src/AtaqueSpam.java
+++ src/AtaqueSpam.java
@@ -1,56 +1,98 @@
 import java.util.List;
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AtaqueSpam {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		// Configuración inicial de web driver
 		System.setProperty("webdriver.chrome.driver", "src\\resources\\drivers\\chromedriver.exe");
-		
-		WebDriver driver = new ChromeDriver();
-		int x = 7;
 
-		for(int p=0;p<=10;p++) {
-		
-		//Registrarse
-		driver.get("https://www.automationexercise.com/login");
-		driver.findElement(By.className("fa-lock")).click();
-		driver.findElement(By.name("name")).sendKeys("AtaqueSpam");
-		//Llenar el campo email correcto
-		List<WebElement> emails = driver.findElements(By.name("email"));
-		emails.get(1).sendKeys(email(x));
-		x += 1;
-		//Tocar el botón correcto
-		List<WebElement> buttoms = driver.findElements(By.className("btn-default"));
-		buttoms.get(1).click();
-		
-	//*******************************************//
-		
-		//Llenar el formulario
-		
-		driver.findElement(By.id("password")).sendKeys("contrasena");
-		driver.findElement(By.id("first_name")).sendKeys("Ataque");
-		driver.findElement(By.id("last_name")).sendKeys("Spam");
-		driver.findElement(By.id("address1")).sendKeys("Alabama");
-		driver.findElement(By.id("state")).sendKeys("Rumania");
-		driver.findElement(By.id("city")).sendKeys("Toronto");
-		driver.findElement(By.id("zipcode")).sendKeys("123");
-		driver.findElement(By.id("mobile_number")).sendKeys("321");
-		
-		driver.findElement(By.className("btn-default")).click();
-		
-	//*******************************************//
-		
-		//Click en botón de continuar
-		
-		driver.findElement(By.className("btn-primary")).click();
+		WebDriver driver = new ChromeDriver();
+		int x = 20;
+		String url = "https://www.automationexercise.com/login";
+
+		// Registrarse
+
+		driver.get(url);
+
+		while (true) {
+			driver.findElement(By.className("fa-lock")).click();
+			driver.findElement(By.name("name")).sendKeys("AtaqueSpam");
+			Thread.sleep(2000);
+
+			// Llenar el campo email correcto
+			if (1 == 1) {
+				List<WebElement> emails = driver.findElements(By.name("email"));
+				emails.get(1).sendKeys(email(x));
+			}
+			x += 1;
+			Thread.sleep(2000);
+			// Tocar el botón correcto
+			List<WebElement> buttoms = driver.findElements(By.className("btn-default"));
+			buttoms.get(1).click();
+
+			// *******************************************//
+
+			// Llenar el formulario
+
+			driver.findElement(By.id("password")).sendKeys("contrasena");
+			Thread.sleep(2000);
+			driver.findElement(By.id("first_name")).sendKeys("Ataque");
+			Thread.sleep(2000);
+			driver.findElement(By.id("last_name")).sendKeys("Spam");
+			Thread.sleep(2000);
+			driver.findElement(By.id("address1")).sendKeys("Alabama");
+			Thread.sleep(2000);
+			driver.findElement(By.id("state")).sendKeys("Rumania");
+			Thread.sleep(2000);
+			driver.findElement(By.id("city")).sendKeys("Toronto");
+			Thread.sleep(2000);
+			driver.findElement(By.id("zipcode")).sendKeys("123");
+			Thread.sleep(2000);
+			driver.findElement(By.id("mobile_number")).sendKeys("321");
+			Thread.sleep(2000);
+
+			driver.findElement(By.className("btn-default")).click();
+
+			// *******************************************//
+
+			// Click en botón de continuar
+
+			driver.findElement(By.className("btn-primary")).click();
+
+			// *******************************************//
+
+			String mainTab = driver.getWindowHandle();
+
+			// Para abrir una nueva pestaña
+			((JavascriptExecutor) driver).executeScript("window.open(arguments[0])", url);
+			Thread.sleep(2000);
+
+			Set<String> handles = driver.getWindowHandles();
+			// Para controlar la pestaña que se acaba de crear
+			for (String actual : handles) {
+				System.out.println("-- Handled ID: " + actual);
+
+				if (!actual.equalsIgnoreCase(mainTab)) {
+					System.out.println("cambiando el tab");
+					driver.switchTo().window(actual);
+				}
+			}
+
 		}
+
 	}
 
+	// *******************************************//
+
+	// Generador de Emails
 
 	public static String email(int x) {
 
@@ -67,5 +109,4 @@ public class AtaqueSpam {
 			j += 1;
 		}
 	}
-
 }
